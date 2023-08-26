f1e69501cb8e4423089b5dc288fce926e5c934cb
BlackcoinDev <dev@blackcoin.dev>
2023-08-26 09:34:23 +0200

    build: update build files

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -283,7 +283,7 @@ public:
         vFixedSeeds = std::vector<uint8_t>(std::begin(chainparams_seed_test), std::end(chainparams_seed_test));
 
         fDefaultConsistencyChecks = false;
-        fRequireStandard = false;
+        fRequireStandard = true;
         m_is_test_chain = true;
         m_is_mockable_chain = false;
 
